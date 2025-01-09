1e589e40a7be17a9e82609ff009196b0c5e7bd42
Botalov Sergey <89090168690@mail.ru>
2025-01-09 22:03:54 +0500

    update

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from app.api_v1 import router as router_api_v1
 
-app = FastAPI(title="FastAPI, Docker, and Traefik")
+app = FastAPI(title="FastAPI, Docker")
 
 app.include_router(router=router_api_v1, prefix="/api/v1")
 
